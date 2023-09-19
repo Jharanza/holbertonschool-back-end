bf8ddcf6752787af70da42591061c1d50a30560f
Jhony Arana <Jharanza1986@gmail.com>
2023-09-19 13:22:29 -0500

    feat: get the information from an API and display it in an orderly manner

---
+++ README.md
@@ -0,0 +1,14 @@
+<center><h1>API</h1></center>
+In this repository we manipulate the API's of differents web pages to obtain information, enter information
+
+<center><h1>Technology</h1></center>
+Language:
+	Python
+	HTMÑL 
+	JSON
+Others:
+	CSV
+	API's
+	REST API's
+	
+THe python files are checked with pycodestyle
---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+employee_id = sys.argv[1]
+
+
+if __name__ == '__main__':
+
+    url_base = 'https://jsonplaceholder.typicode.com/'
+
+    ''' Get the employee information and transform it in json format '''
+    response_employee = requests.get(f'{url_base}users/{employee_id}')
+    employee_data = response_employee.json()
+
+    ''' Get the todo list information and transform it in json format '''
+    response_todo_list = requests.get(f'{url_base}todos/?userId={employee_id}')
+    todo_list_data = response_todo_list.json()
+
+    ''' Number of task finished and total tasks '''
+    len_completed = len([task for task in todo_list_data if task['completed']])
+    total_tasks = len(todo_list_data)
+
+    ''' Display the progress '''
+    print(f"Employee {employee_data['name']} is done with tasks\
+            ({len_completed}/{total_tasks}):")
+    for task in todo_list_data:
+        print(f"\t {task['title']}")
+
+    if response_employee.status_code == 200:
+        content = response_employee.content
