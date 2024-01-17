2630193a10c800be722c97b043fb79721437a9b2
Jhony Arana <jharanza1986@gmail.com>
2024-01-16 20:35:25 -0500

    feat: Extract data from an API and sort it

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+''' Module that extract data from an API '''
+from sys import argv
+import requests
+
+
+def get_progress(employee_id) -> None:
+    """ Method that take 2 endpoints and extract data about the same user """
+
+    url = 'https://jsonplaceholder.typicode.com'  # Principal root
+
+    ''' Info to the endpoint users '''
+    users = requests.get(f"{url}/users/{employee_id}")
+    users_data = users.json()
+
+    ''' Info to the endpoint to_do'''
+    todos = requests.get(f"{url}/todos?userId={employee_id}")
+    todos_data = todos.json()
+
+    '''Tasks completed'''
+    completed_task = [task for task in todos_data if task['completed']]
+
+    ''' All tasks'''
+    tasks = [task for task in todos_data]
+
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(users_data['name'], len(completed_task), len(tasks)))
+
+    for task in completed_task:
+        print('\t {}'.format(task['title']))
+
+
+if __name__ == '__main__':
+    try:
+        employee_id = int(argv[1])  # assign arg in position 2 to the variable
+    except ValueError:
+        print(ValueError)
+    get_progress(employee_id)
---
+++ api/README.md
@@ -0,0 +1,24 @@
+<h1>API</h1>
+    <p>In this project we learn about how to get info from an api, how to sort it and how we can manipulate this data</p>
+
+<h2>Learning Objectives</h2>
+    <p>What Bash scripting should not be used for</p>
+    <p>What is an API</p>
+    <p>What is a REST API</p>
+    <p>What are microservices</p>
+    <p>What is the CSV format</p>
+    <p>What is the JSON format</p>
+    <p>Pythonic Package and module name style</p>
+    <p>Pythonic Class name style</p>
+    <p>Pythonic Variable name style</p>
+    <p>Pythonic Function name style</p>
+    <p>Pythonic Constant name style</p>
+    <p>Significance of CapWords or CamelCase in Python</p>
+
+<ul>Tasks</ul>
+    <li>0.Gather data from an API<li>
+    <li>1. Export to CSV<li>
+    <li>2. Export to JSON<li>
+    <li>3. Dictionary of list of dictionaries<li>
+
+    
